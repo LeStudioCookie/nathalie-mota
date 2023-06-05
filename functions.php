13296fa33752dafe2e51279ae5940d241d12dd89
LeStudioCookie <lestudiocookie@gmail.com>
2023-06-05 18:03:05 +0200

    Modification pour creation theme personnalise

--- functions.php
+++ functions.php
@@ -1,4 +1,18 @@
 <?php 
+/**
+ * Enqueue scripts and styles.
+ *
+ * @return void
+ */
+function nathalie_mota_scripts() {
+
+	
+	wp_enqueue_style( 'theme', get_template_directory_uri() . '/style.css', array(), '0.0.1');
+
+
+}
+add_action( 'wp_enqueue_scripts', 'nathalie_mota_scripts' );
+
 
 //ajout d'une nouvelle zone de menu "header" et "footer" au thème
 
@@ -10,3 +24,7 @@ function register_my_menus() {
     );
    }
    add_action( 'init', 'register_my_menus' );
+
+
+
+   
